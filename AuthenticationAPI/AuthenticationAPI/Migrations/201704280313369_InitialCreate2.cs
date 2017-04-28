056e8b5fe222fec5ec9ddfac918566af7e135f82
Abu Rafe Md. Jamil <Abu Rafe Md. Jamil>
2017-04-28 18:27:45 +0600

    SPL user manual without proper color

---
+++ AuthenticationAPI/AuthenticationAPI/Migrations/201704280313369_InitialCreate2.cs
@@ -0,0 +1,18 @@
+namespace AuthenticationAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "UserRole", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "UserRole");
+        }
+    }
+}
